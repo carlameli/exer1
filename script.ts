f8878ab8da53c62f0bc0f1ea9c69ccaddf2dc9c5
carlameli <carla.pilots@gmail.com>
2021-10-28 21:51:21 -0300

    Correção

--- script.ts
+++ script.ts
@@ -1,30 +1,22 @@
-window.onload=function(){
+window.onload = function (): void {
     const button = document.getElementById("submit");
-    
     const input = document.getElementById('palavra') as HTMLInputElement;
     if (button && input) {
         button.onclick = function () {
-            window.alert('Total de Vogais: ' + totalVogais(input['value']));
+            window.alert('Total de Vogais: cd ..' + totalVogais(input.value));
         }
-        
     }
-
 }
 
-
-let totalVogais = function (palavra: string) {
+let totalVogais = function (palavra: string): number {
     let total = 0;
     let vogais = "aeiou"
     for (let index = 0; index < palavra.length; index++) {
         const letra = palavra[index].toLowerCase();
-       
         if (vogais.includes(letra)) {
             total++
-            
         }
-        
     }
     return total;
-    
 }
-console.log( totalVogais("Acessibilidade"))
+console.log(totalVogais("Acessibilidade"))
